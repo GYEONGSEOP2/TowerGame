5c18476ada40e3f0cb22c34caffd744a01684e37
GYEONGSEOP2 <lllsdll@nate.com>
2026-09-06 22:44:39 +0900

    Remove compile error

--- Assets/Scripts/TowerAttack.cs
+++ Assets/Scripts/TowerAttack.cs
@@ -3,7 +3,7 @@ using Unity.Collections;
 using System.Collections.Generic;
 using Unity.Entities;
 using Unity.Mathematics;
-using Unity.Rendering;ㅁ
+using Unity.Rendering;
 using Unity.Transforms;
 using UnityEngine;
 
